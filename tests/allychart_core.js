57c7946dcd68a9f95608db6252d87ae100b2654e
Patrick McElhaney <Patrick.McElhaney@ally.com>
2011-01-03 15:14:35 -0500

    Test "set chart container." Now we're rockin'!

--- tests/allychart_core.js
+++ tests/allychart_core.js
@@ -1,12 +1,17 @@
 (function($) {
 
-module("allychart: core")
+module("allychart: core");
 
 test("set width and height", function() {	
   $('#chart-target').allyChart({width: 200, height: 100, sourceTable:'#empty-table' });
   var chart = Highcharts.chart;
   equals(200, chart.options.chart.width);
   equals(100, chart.options.chart.height);
+});
+
+test("set chart container", function() {	
+  $('#chart-target').allyChart({sourceTable:'#empty-table' });
+  equals(Highcharts.chart.options.renderTo, document.getElementById('#chart-target'));
 });
 
 })(jQuery);
