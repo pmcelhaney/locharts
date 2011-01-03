5908024d45178c035315eebaa3bea6142626aa68
Patrick McElhaney <Patrick.McElhaney@ally.com>
2011-01-03 15:20:10 -0500

    Test that the margin is set.

--- tests/allychart_core.js
+++ tests/allychart_core.js
@@ -2,11 +2,12 @@
 
 module("allychart: core");
 
-test("set width and height", function() {	
-  $('#chart-target').allyChart({width: 200, height: 100, sourceTable:'#empty-table' });
+test("set width, height, and margin", function() {	
+  $('#chart-target').allyChart({width: 200, height: 100, margin: [10,20,30,40], sourceTable:'#empty-table' });
   var chart = Highcharts.chart;
   equals(200, chart.options.chart.width);
   equals(100, chart.options.chart.height);
+  same([10,20,30,40], chart.options.chart.margin);
 });
 
 test("set chart container", function() {	
