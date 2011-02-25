0267db1836418eba7105e45c6a5a6d558e332949
Patrick McElhaney <patrick.mcelhaney@ally.com>
2011-02-24 22:47:27 -0500

    Fleshing out the API some more

--- jquery.ui.ally.barchart.js
+++ jquery.ui.ally.barchart.js
@@ -78,14 +78,35 @@
     - textLabel()
     - hoverLabel()
     - draw() 
- 
-   XAxis([values])
-    - draw()
-    - label() 
 
-   YAxis(min, max) 
-    - draw() 
-    - label()
+   Chart 
+     - xAxis : typically an CategoryAxis
+     - yAxis : typically a ScalarAxis
+     - width()
+ 	 - height()
+     - margin()
+     - plotWidth()
+     - plotHeight()
+     - getCoordinatesForPoint(point)
+     - eachSeries()
+     - addSeries()
+
+   Series : BarSeries, AreaSeries, LineSeries, etc.
+     - points()
+     - draw()
+
+   Axis(x_or_y)
+    - draw() // also draws lines
+    - formatLabel() 
+    - categories() : for scalar, it's the places where lines are drawn
+    - count() : the number of ticks
+    - min() : scaler only
+    - max() : scaler only
+    - isScaler() 
+
+
+
+
 
 
 BarChartPoint = {
@@ -111,6 +132,47 @@ options.Point is an object that extends BarChartPoint (using prototypal inherita
 is an object that extends options.Point. That's a little complicated, but it's efficient, because there's
 only one copy of each function. And that's how prototypal inheritance is designed to work.
 
+BarChartPoint.draw = function (chart) {
+  // uses chart.yAxis.min(), chart.yAxis.max(), chart.xAxis.count(), 
+  // chart.margin(),  chart.plotWidth(), and chart.plotHeight()
+  
+  // Or it could use chart.getCoordinatesForPoint(this) to figure out where the top of the bar is
+  // and chart.plotWidth()  / chart.xAxis.count() to find out how wide it should be
+}
+
+{
+   Point: {
+      textLabel: function () { ... },
+      hoverLabel: ...,
+      onmMouseOver: ...
+   },
+    
+   chart: {
+      width: function () { return 600 },
+      height: function () { return 400 }
+     
+   },
+
+   xAxis: {
+      labels: function() { ... }
+   } 
+
+   yAxis: {
+      min: function () {},
+      max: function () {},
+      isScaler: function () { return true;}
+   }
+}
+
+Go through the options object, and whenever a simple value is encountered, wrap it in a function.
+e.g., width: 400 becomes width: function () { return 400 }
+
+addSeries() can be a function on the widget itself. It would have one argument, an array. If the item in 
+the array is a number, it's converted to on object { index: positionInArray, value: theNumber }. Otherwise,
+it should be already be an object with those fields. The object's prototype is assigned to Point, so that
+the series always contains an array of Points.
+
+Possibly a second argument of addSeries is the type of series (bar, line, area, etc.)
 
 
 */
