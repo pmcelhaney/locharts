e4cfb463393177234d3eeaa16b1b0bb4a83d5465
Patrick McElhaney <patrick.mcelhaney@ally.com>
2011-03-02 22:46:01 -0500

    Factored out axis object

--- jquery.ui.allychart-tdd.js
+++ jquery.ui.allychart-tdd.js
@@ -7,21 +7,26 @@
 
 
 (function( $, Rapahel, undefined ) {
+	var Axis = {
+		length: function () {
+			return this.max - this.min;
+		},
+		scale: function () {
+			return (this.end - this.start) / this.length();
+		},
+		mapValue: function (value) {
+			return this.start + this.scale() * (value - this.min);
+		}
+	}
+	
+	
 	
 	$.widget("ui.allyChart", {		
 		_create: function() {	
 			var yAxis = this.options.yAxis;
-			yAxis.length = function () {
-				return this.max - this.min;
-			};
 			yAxis.start = this.options.height;
 			yAxis.end = 0;
-			yAxis.scale = function () {
-				return (this.end - this.start) / this.length();
-			};
-			yAxis.mapValue = function (value) {
-				return this.start + this.scale() * (value - this.min);
-			};
+			$.extend(this.options.yAxis, Axis);
 		},
 		points: function () {
 			var yAxis = this.options.yAxis;
