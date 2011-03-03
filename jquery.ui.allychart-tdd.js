c43f8ec2ea62f3daa61a963890bc3b53c3f0a48e
Patrick McElhaney <patrick.mcelhaney@ally.com>
2011-03-02 22:26:01 -0500

    First real(ish) test.

--- jquery.ui.allychart-tdd.js
+++ jquery.ui.allychart-tdd.js
@@ -9,7 +9,14 @@
 (function( $, Rapahel, undefined ) {
 	
 	$.widget("ui.allyChart", {		
-		_create: function() {}
+		_create: function() {			
+		},
+		points: function () {
+			var yAxisSize = this.options.yAxis.max - this.options.yAxis.min;
+			var yScale = this.options.height / yAxisSize;
+			var y = this.options.height - (yScale * (this.options.values[0] - this.options.yAxis.min));
+			return [y];
+		}
 	});
 	
 }(jQuery, Raphael));
--- tests/allychart-tdd.js
+++ tests/allychart-tdd.js
@@ -4,7 +4,23 @@ module("allychart: core");
 
 test("sanity check", function () {
 	equals(1, 1);
-})
+});
+
+
+test("A yValue should map to the correct y coordinate.", function () {
+	$('#chart-target').allyChart({ 
+		height: 100, 
+		values: [30],
+		yAxis: {
+			min: 20, 
+			max: 40
+		} 
+	
+	});
+	var points = $('#chart-target').allyChart('points');
+	
+	equals(points[0], 50);
+});
 
 })(jQuery);
 
