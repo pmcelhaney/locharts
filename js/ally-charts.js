9701d62c809eedb64c42fc504c6e9084e720e9ef
Patrick McElhaney <patrick.mcelhaney@ally.com>
2011-04-19 15:52:07 -0400

    Grid.xForIndex()

--- js/ally-charts.js
+++ js/ally-charts.js
@@ -1,4 +1,15 @@
-window.Grid = function () {
+window.Grid = function (options) {
+	var options = options || {};
+	var marginLeft = options.marginLeft || 0;
+	var marginRight = options.marginRight || 0;
+	var width = ( options.width || 960 ) - marginLeft - marginRight;
+	var columnCount = ( options.xLabels && options.xLabels.length ) || 1;
+
 	
-	return {};
+	return {
+		xForIndex: function (i) {
+			return (i+1) * width/(columnCount+1) + marginLeft;
+		}		
+	};
 };
+
--- test/ally-charts-spec.js
+++ test/ally-charts-spec.js
@@ -1,10 +1,24 @@
 describe("Grid", function() {
-
-	it("should create an object", function() {
-		var grid = Grid();
-		expect(typeof grid).toEqual("object");
-	});
+/*
+- xForIndex(i), yForIndex(i)
+- xForValue(v), yForValue(v)
+- columnWidth(), rowHeight()
+*/
 	
+	it("should know the x-coordinate for a given index", function () {
+		expect(Grid().xForIndex(0)).toEqual(480);
+		
+		expect(Grid({ width: 60 }).xForIndex(0)).toEqual(30);
+				
+		expect(Grid({ width: 60, xLabels: ['one', 'two'] }).xForIndex(0)).toEqual(20);
+		expect(Grid({ width: 60, xLabels: ['one', 'two'] }).xForIndex(1)).toEqual(40);
+		
+		expect(Grid({ width: 60, xLabels: ['one', 'two', 'three'] }).xForIndex(0)).toEqual(15);
+		expect(Grid({ width: 60, xLabels: ['one', 'two', 'three'] }).xForIndex(1)).toEqual(30);
+		expect(Grid({ width: 60, xLabels: ['one', 'two', 'three'] }).xForIndex(2)).toEqual(45);
+		
+		expect(Grid({ width: 160, xLabels: ['one', 'two'], marginLeft: 20, marginRight: 20 }).xForIndex(0)).toEqual(60);
+	});
 });
 
 
