737c9f5411f178d3e9f2be7742ddeb72b582f011
Patrick McElhaney <patrick.mcelhaney@ally.com>
2011-04-20 14:57:16 -0400

    Simple prototype now has bars and labelled values.

--- js/brush-prototypes.js
+++ js/brush-prototypes.js
@@ -4,18 +4,33 @@ bars = function (paper, grid, data) {
 		var left = grid.xForIndex(i) - (width / 2); 
 		var top = grid.yForValue(this);
 		var height = grid.yForBottomEdge() - top;
-		paper.rect(left, top, width, height);
+		paper.rect(left, top, width, height).translate(0.5, 0.5).attr('fill', '#eee');
 		
-		console.log(grid.xForIndex(i), left, top, width, height);
+	});
+};
+
+xAxis = function (paper, grid, data) {
+	$(data).each(function (i) {
+		paper.text(grid.xForIndex(i), grid.yForBottomEdge() + 10, this);
 	});
 };
 
 $(function() {
 	var paper = Raphael(50, 50, 600, 400);
-	var grid = Grid({width: 600, height: 400, yMinValue: 0, yMaxValue: 100, xLabels: ['Jan', 'Feb', 'Ma']});
-
+	var grid = Grid({
+		width: 600, 
+		height: 400, 
+		yMinValue: 0, 
+		yMaxValue: 50, 
+		xLabels: ['Jan', 'Feb', 'Mar'],
+		marginBottom: 20,
+		marginTop: 10,
+		marginLeft: 10,
+		marginRight: 10
+	});
+	var data = [10, 20, 40];
 	
-	bars(paper, grid, [10, 20, 40]);
-	
+	bars(paper, grid, data);
+	xAxis(paper, grid, data);
 	
 });
