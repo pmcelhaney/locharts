08dc5c4eeec8044bc59b11cba0bf5da199e8d541
Patrick McElhaney <patrick.mcelhaney@ally.com>
2011-04-28 17:06:10 -0400

    Added some styling to make the prototype look better.

--- js/layers.js
+++ js/layers.js
@@ -1,8 +1,15 @@
 define(function () {
 var GRADIENTS = {
-	BLUE: '270-rgba(55,152,199,1)-rgba(70,195,255,.5)',
+	BLUE: '270-rgba(55,152,199,0.1)-rgba(70,195,255,0.1)',
 	PURPLE: '270-rgb(101,3,96)-rgb(211,6,201)'
 };
+
+var COLORS = {
+	LINES: '#DCDFE3',
+	TEXT: '#645050'
+};
+
+var FONT = 'Verdana, Sans-Serif';
 
 
 return {
@@ -15,12 +22,12 @@ return {
 			var left = grid.xForIndex(i) - (width / 2); 
 			var top = grid.yForValue(datum);
 			var height = grid.yForBottomEdge() - top;
-			var bar = paper.rect(left + 0.5, top + 0.5, width, height).attr('fill', GRADIENTS.BLUE).attr('stroke-width', 0);
+			var bar = paper.rect(left + 0.5, top + 0.5, width, height).attr('fill', GRADIENTS.BLUE).attr('stroke-width', 0).attr('fill-opacity', 0.5);
 			bar.hover(function () {
 				$(paper).trigger('focusItem.chart', [i, datum]);
-				this.attr('fill', GRADIENTS.PURPLE);
+				this.attr('fill', GRADIENTS.PURPLE).attr('fill-opacity', 1);
 			}, function () {
-				this.attr('fill', GRADIENTS.BLUE);
+				this.attr('fill', GRADIENTS.BLUE).attr('fill-opacity', 0.5);
 			});
 		});
 	},
@@ -29,7 +36,7 @@ return {
 		var paper = this.paper;
 		var grid = this.grid;
 		$(this.data).each(function (i) {
-			paper.text(grid.xForIndex(i), grid.yForBottomEdge() + 10, grid.xLabelForIndex(i));
+			paper.text(grid.xForIndex(i), grid.yForBottomEdge() + 10, grid.xLabelForIndex(i)).attr('fill', COLORS.TEXT);
 		});
 	},
 
@@ -37,7 +44,7 @@ return {
 		var paper = this.paper;
 		var grid = this.grid;
 		$(this.data).each(function (i) {
-			paper.text(grid.xForIndex(i), grid.yForValue(this) - 10, this);
+			paper.text(grid.xForIndex(i), grid.yForValue(this) - 10, this).attr('font', FONT).attr('fill', COLORS.TEXT);
 		});
 	},
 
@@ -48,7 +55,7 @@ return {
 		for (i = 1; i < this.data.length; i++) {
 			var x = 0.5 + Math.round(grid.xForIndex(i) - grid.columnWidth() / 2);
 			var y = 0.5 + grid.yForBottomEdge();
-			paper.path('M' + x + ' ' + y + 'L' + x + ' ' + (y+10) );
+			paper.path('M' + x + ' ' + y + 'L' + x + ' ' + (y+10) ).attr('stroke', COLORS.LINES);
 		}
 	},
 
@@ -62,23 +69,31 @@ return {
 		for (i = Math.floor(grid.yMinValue()); i < grid.yMaxValue() ; i += increment) {
 			if (i > grid.yMinValue()) {
 				y = 0.5 + grid.yForValue(i);
-				paper.path('M' + (grid.xForLeftEdge() + 0.5) + ' ' + y + 'L' + (grid.xForRightEdge() + 0.5) + ' ' + y ).attr('stroke', '#eee').attr('z-index', 0);
-				paper.text(grid.xForLeftEdge() + 0.5 - 5, grid.yForValue(i), i + '').attr('text-anchor', 'end');
+				paper.path('M' + (grid.xForLeftEdge() + 0.5) + ' ' + y + 'L' + (grid.xForRightEdge() + 0.5) + ' ' + y ).attr('stroke', COLORS.LINES).attr('z-index', 0);
+				paper.text(grid.xForLeftEdge() + 0.5 - 5, grid.yForValue(i), i + '').attr('text-anchor', 'end').attr('font', FONT).attr('fill', COLORS.TEXT);
 			}
 		}
 	},
 
 	'borders': function () {
-		this.paper.rect(this.grid.xForLeftEdge() + 0.5, this.grid.yForTopEdge() + 0.5, this.grid.width(), this.grid.height());
+		var rect = this.paper.rect(this.grid.xForLeftEdge() + 0.5, this.grid.yForTopEdge() + 0.5, this.grid.width(), this.grid.height());
+		rect.attr('fill', '#FBFBFB');
+		rect.attr('stroke', COLORS.LINES);
+		
 	},
 	
 
 	'bubble': function () {
-		var bubble = this.paper.rect(this.grid.xForIndex(0) - 29.5, this.grid.yForValue(this.data[0]) - 60.5, 60, 40, 10).attr('fill', '#fff');
+		var width = 100;
+		var height = 40;
+		var bubble = this.paper.rect(this.grid.xForIndex(0) - width / 2 + 0.5, this.grid.yForValue(this.data[0]) - height - 20.5, width, height, 5)
+			.attr('fill', '#fff')
+			.attr('stroke', COLORS.LINES)
+			.attr('stroke-width', 2);
 		var grid = this.grid;
 		var data = this.data;
 		$(this.paper).bind('focusItem.chart', function (event, index, datum) {
-			bubble.animate({x: grid.xForIndex(index) - 29.5, y: grid.yForValue(datum) - 60.5 }, 300, "<>");
+			bubble.animate({x: grid.xForIndex(index) - width / 2 + 0.5, y: grid.yForValue(datum) - height - 20.5 }, 300, "<>");
 		});
 	}
 
