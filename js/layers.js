582b43c7bed395023288693fd96cb2ceedaf9522
Patrick McElhaney <patrick.mcelhaney@ally.com>
2011-05-10 15:53:00 -0400

    Found out that Raphael.path() can take an array rather than a string, which is a lot easier.

--- js/layers.js
+++ js/layers.js
@@ -177,10 +177,10 @@ return {
 		var previousY = grid.yForBottomEdge();
 				
 		$(data).each(function (i) {
-			var path = 'M' + ( grid.xForIndex(i) + 0.5 ) + ' ' + ( grid.yForValue(data[i][i]) + 0.5 );
-			path += 'L' + (grid.xForIndex(data.length) + 0.5) + ' ' + (grid.yForValue(data[i][data.length]) + 0.5);
-			path += 'L' + ( grid.xForIndex(data.length) + 0.5 ) + ' ' + (grid.yForValue(data[Math.max(0, i-1)][data.length]) + 0.5);
-			path += 'Z';
+			var path = ['M',  ( grid.xForIndex(i) + 0.5 ), ( grid.yForValue(data[i][i]) + 0.5 ) ];
+			path.push('L', (grid.xForIndex(data.length) + 0.5), (grid.yForValue(data[i][data.length]) + 0.5) );
+			path.push('L', ( grid.xForIndex(data.length) + 0.5 ), (grid.yForValue(data[Math.max(0, i-1)][data.length]) + 0.5) );
+			path.push('Z');
 			paper.path(path).attr({  fill: grid.fillColor(i),  'stroke-width': 0, opacity: 0.4  });
 		});
 		
