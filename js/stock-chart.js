08b903f88d912ac99cde0294e88994eff331ad06
Patrick McElhaney <patrick.mcelhaney@ally.com>
2011-05-19 13:30:15 -0400

    Refactored a bit, and went back to loading random data because it's fast.

--- js/stock-chart.js
+++ js/stock-chart.js
@@ -1,71 +1,113 @@
-var TradingDay = function (v, o, h, l, c, d) {
-    
-    return {
-        high: h,
-        low: l,
-        open: o,
-        close: c,
-        date: d,
-        volume: v,
-        valueOf: function () { return this.close; }      
-    };
-};
-
-
-var randomTradingDays = function (n) {
-    var i;
-    var tradingDays = [];
-    var day;
-    var lastClose = 30;
-    var previousDate = new Date(2010,01,04);
-    for (i = 0; i<n; i++) {
-        day = TradingDay();
-        day.open = lastClose + ( Math.pow(1 + Math.random(), 2) - Math.pow(1 + Math.random(), 2) ) / 10;
-        day.high = lastClose + Math.pow(1 + Math.random() * 0.5, 3);
-        day.low  = Math.max(Math.random(), lastClose - Math.pow(1 + Math.random() * 0.5, 3));
-        day.close = day.low + Math.random() * (day.high - day.low);
-        day.date = new Date(previousDate.getTime() + 24 * 60 * 60 * 1000 * (previousDate.getDay() == 6 ? 3 : 1));
-	    day.volume = 1000 + Math.round( Math.sqrt(Math.random() * 50000 * 50000) );
-        tradingDays.push(day);
-        previousDate = day.date;
-        lastClose = day.close;
-        
-    }
-    
-    return tradingDays;
-};
-
-
-var formatDate = function(d) {
-	var twoDigits = function(n) {
-		return n > 9 ? n : '0' + n;
-	};
-	if (!d.getMonth) {
-		return d;
-	}
-	return twoDigits(d.getMonth() + 1) + '/' + twoDigits(d.getDate()) + '/' + d.getFullYear();		
-};
-
-
-
-
 define(['chart', 'Money'], function (chart, Money) {
 
+    var TradingDay = function (v, o, h, l, c, d) {
 
-	var COLORS = {
-		LINES: '#DCDFE3',
-		TEXT: '#645050',
-		FILL: 'rgb(55,152,199)'
-	};
+        return {
+            high: h,
+            low: l,
+            open: o,
+            close: c,
+            date: d,
+            volume: v,
+            valueOf: function () { return this.close; }      
+        };
+    };
 
+
+    var randomTradingDays = function (n) {
+        var i;
+        var tradingDays = [];
+        var day;
+        var lastClose = 30;
+        var previousDate = new Date(2010,01,04);
+        for (i = 0; i<n; i++) {
+            day = TradingDay();
+            day.open = lastClose + ( Math.pow(1 + Math.random(), 2) - Math.pow(1 + Math.random(), 2) ) / 10;
+            day.high = lastClose + Math.pow(1 + Math.random() * 0.5, 3);
+            day.low  = Math.max(Math.random(), lastClose - Math.pow(1 + Math.random() * 0.5, 3));
+            day.close = day.low + Math.random() * (day.high - day.low);
+            day.date = new Date(previousDate.getTime() + 24 * 60 * 60 * 1000 * (previousDate.getDay() == 6 ? 3 : 1));
+    	    day.volume = 1000 + Math.round( Math.sqrt(Math.random() * 50000 * 50000) );
+            tradingDays.push(day);
+            previousDate = day.date;
+            lastClose = day.close;
+
+        }
+
+        return tradingDays;
+    };
+
+
+    var formatDate = function(d) {
+    	var twoDigits = function(n) {
+    		return n > 9 ? n : '0' + n;
+    	};
+    	if (!d.getMonth) {
+    		return d;
+    	}
+    	return twoDigits(d.getMonth() + 1) + '/' + twoDigits(d.getDate()) + '/' + d.getFullYear();		
+    };
+    
+
+    
 
 	$(function() {
+        var data, volumeData;  
 
-		
-		
+        var loadFromYahoo = function () {
+            $('#candlestick').html("Loading data from Yahoo... (on the live site it will be pre-cached and fast)");
 
-  
-       
+            $.ajax({
+                url: 'http://query.yahooapis.com/v1/public/yql',
+                data: {
+                    q: 'select * from yahoo.finance.historicaldata where symbol = "AAPL" and startDate ="2010-05-18" and endDate="2011-05-17" | sort(field="Date")',
+                    format: 'json',
+                    env: 'http://datatables.org/alltables.env'          
+                },
+                dataType: 'jsonp',
+                success: function (yqlData) {  
+                    data = $(yqlData.query.results.quote).map(function () { 
+                        return { 
+                            date: this.Date, 
+                            volume: +this.Volume, 
+                            open: +this.Open,
+                            close: +this.Close,
+                            high: +this.High,
+                            low: +this.Low,
+                            valueOf: function () { return this.close; } 
+                        };
+                    }).toArray();
+                    volumeData = $(data).map(function () { 
+                        return { 
+                            date: this.date, 
+                            volume: this.volume, 
+                            open: this.open,
+                            close: this.close,
+                            high: this.high,
+                            low: this.low,
+                            valueOf: function () { return this.volume; } 
+                        }; 
+                    } ).toArray();
+                    $('#candlestick').html('');
+                    drawStockChart(data, volumeData, data);
+                }
+            });            
+        };
+		
+		var loadFromRandom = function () {
+		    data = randomTradingDays(500);
+            volumeData = $(data).map(function () { return { 
+                date: this.date, 
+                volume: this.volume, 
+                open: this.open,
+                close: this.close,
+                high: this.high,
+                low: this.low,
+                valueOf: function () { return this.volume; } 
+            }; } ).toArray();
+            
+            drawStockChart(data.slice(-90), volumeData.slice(-90), data); 
+		};
 		
 		var drawStockChart = function (data, volumeData, allData) {
 
@@ -139,68 +181,9 @@ define(['chart', 'Money'], function (chart, Money) {
 
         };
         
-        var data, volumeData, allData;
-        
-        //         var data = randomTradingDays(500);
-        //         var volumeData = $(data).map(function () { return { 
-        //     date: this.date, 
-        //     volume: this.volume, 
-        //     open: this.open,
-        //     close: this.close,
-        //     high: this.high,
-        //     low: this.low,
-        //     valueOf: function () { return this.volume; } 
-        // }; } ).toArray();
-        //          
-        
-   
-        
-        $('#candlestick').html("Loading data from Yahoo... (on the live site it will be pre-cached and fast)");
-        
-        
-        var url = "http://query.yahooapis.com/v1/public/yql?";
-        
-        
-        $.ajax({
-          url: 'http://query.yahooapis.com/v1/public/yql',
-          data: {
-            q: 'select * from yahoo.finance.historicaldata where symbol = "AAPL" and startDate ="2010-05-18" and endDate="2011-05-17" | sort(field="Date")',
-            format: 'json',
-            env: 'http://datatables.org/alltables.env'          
-          },
-          
-          dataType: 'jsonp',
-          success: function (yqlData) {  
-              
-            data = $(yqlData.query.results.quote).map(function () { 
-                    return { 
-                        date: this.Date, 
-                        volume: +this.Volume, 
-                        open: +this.Open,
-                        close: +this.Close,
-                        high: +this.High,
-                        low: +this.Low,
-                        valueOf: function () { return this.close; } 
-                    };
-               }).toArray();
-               
-               
-             volumeData = $(data).map(function () { return { 
-                   date: this.date, 
-                   volume: this.volume, 
-                   open: this.open,
-                   close: this.close,
-                   high: this.high,
-                   low: this.low,
-                   valueOf: function () { return this.volume; } 
-              }; } ).toArray();
-              
-              
-              $('#candlestick').html('');
-              drawStockChart(data, volumeData, data); 
-          }
-        });
-        
+
+        loadFromRandom();
+        //loadFromYahoo();
         
         $('#update-chart-form').submit(function (e) {
             e.preventDefault();
