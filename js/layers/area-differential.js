d2a69b117e28be0c030b1ec6a3e1ee46fff06dba
Patrick McElhaney <patrick.mcelhaney@ally.com>
2011-10-03 21:42:02 -0400

    Fixes the differential area chart.

--- js/layers/area-differential.js
+++ js/layers/area-differential.js
@@ -1,15 +1,36 @@
 define(function () {
 
+
+	
+
 	return  function () {
 		var paper = this.paper;
 		var grid = this.grid;
 		var data = this.data;	
 
+		var getX = function (index) {
+			return grid.xForIndex(index) + 0.5;
+		};
+		
+		var getY = function (series, index) {
+			if (series < 0) return grid.yForBottomEdge();
+			return grid.yForValue(data[series][index]) + 0.5;
+		};
+
+
 		$(data).each(function (i) {
-			var path = ['M',  ( grid.xForIndex(i) + 0.5 ), ( grid.yForValue(data[i][i]) + 0.5 ) ];
-			path.push('L', (grid.xForIndex(data.length) + 0.5), (grid.yForValue(data[i][data.length]) + 0.5) );
-			path.push('L', ( grid.xForIndex(data.length) + 0.5 ), (grid.yForValue(data[Math.max(0, i-1)][data.length]) + 0.5) );
-			path.push('Z');
+		
+			var path = ['M',  getX(i), getY(i, i) ]; // left 
+			var j = i;
+			
+			while (j++ <= data.length) {
+				path.push('L', getX(j), getY(i, j) ); // interior top
+			}
+
+			while (j-- >= i) {
+				path.push('L', getX(j), getY(i-1, j)  ); // interior bottom
+			}
+
 			paper.path(path).attr({	fill: grid.fillColor(i), 'stroke-width': 0, opacity: 0.4 });
 		});
 
