19e0bb48479645e42e4c17d7f349fe96ec2fe559
Patrick McElhaney <patrick.mcelhaney@ally.com>
2012-01-24 14:50:43 -0500

    Added almond and an app.build.js

---
+++ app.build.js
@@ -0,0 +1,11 @@
+({
+    appDir: ".",
+    baseUrl: "./js",
+
+        name: "almond",
+        include: ["examples"],
+
+    optimize: "none",
+    out: "all-in-one-from-file.js",
+    wrap: true
+})
