2b200199239343f8901e5ca4ee0d1734bf73839e
Patrick McElhaney <patrick.mcelhaney@ally.com>
2012-02-01 10:49:16 -0500

    Modified build script to build a single file that has: - the chart, - all of the examples, and - Almond - wrapped in an IIFE
    It's pretty simple to create a build script that contains just the chart,
    the IR stock chart and all of its dependencies, etc.

---
+++ chart-with-examples.build.js
@@ -0,0 +1,9 @@
+({
+    appDir: "./js",
+    baseUrl: "./",
+    name: "almond",
+    include: "chart-examples/examples",
+    optimize: "none",            
+    wrap: true,
+    out: "chart-with-examples.js"
+})
