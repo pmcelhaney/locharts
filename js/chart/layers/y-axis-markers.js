98a2ca30b9ce5b7c94ed5adec7747900587fded6
Patrick McElhaney <patrick.mcelhaney@ally.com>
2012-02-10 13:44:41 -0500

    Updated yAxisMarkers to use the new grid.yValuesForGridlines() function.

--- js/chart/layers/y-axis-markers.js
+++ js/chart/layers/y-axis-markers.js
@@ -6,29 +6,26 @@ define(['./colors'], function (COLORS) {
 		var grid = this.grid;
 		var element = this.element;
 		var i, y;
-		var roughIncrement = Math.round( ( grid.yMaxValue() - grid.yMinValue() ) / (numberOfRows || 5));
-		var roundNumber = Math.pow(10, (Math.floor(Math.log(roughIncrement) / Math.LN10)) );
-		var increment = roughIncrement - (roughIncrement % roundNumber);      
+
+		var yValues = grid.yValuesForGridLines();
 		
-		for (i = Math.floor(grid.yMinValue()); i < grid.yMaxValue() ; i += increment) {
-			if (i > grid.yMinValue()) {
-				y = 0.5 + grid.yForValue(i);
-				//draws the horizontal background lines, should probably be optional
-				paper.path('M' + (grid.xForLeftEdge() + 0.5) + ' ' + y + 'L' + (grid.xForRightEdge() + 0.5) + ' ' + y ).attr('stroke', COLORS.LINES).attr('z-index', 0);
-				var cssProperties = { 
-				    position: 'absolute', 
-				    top: grid.yForValue(i) - 7
-				}; 
-				if (position === 'right') {
-				    cssProperties['right'] = 0;     
-				    cssProperties['width'] = grid.outerWidth() - grid.xForRightEdge() - 5;  
-				} else {
-				    cssProperties['left'] = 0;    
-				    cssProperties['width'] = grid.xForLeftEdge() - 5; 
-				}                            
-				
-				$('<span class="y-axis-label">' + formatter(i).toString() + '</span>').css(cssProperties).appendTo(this.container);  
-			}
+		for (i = 0; i < yValues.length; i++ ) {        
+			y = 0.5 + grid.yForValue(yValues[i]);
+			//draws the horizontal background lines, should probably be optional
+			paper.path('M' + (grid.xForLeftEdge() + 0.5) + ' ' + y + 'L' + (grid.xForRightEdge() + 0.5) + ' ' + y ).attr('stroke', COLORS.LINES).attr('z-index', 0);
+			var cssProperties = { 
+			    position: 'absolute', 
+			    top: y - 7
+			}; 
+			if (position === 'right') {
+			    cssProperties['right'] = 0;     
+			    cssProperties['width'] = grid.outerWidth() - grid.xForRightEdge() - 5;  
+			} else {
+			    cssProperties['left'] = 0;    
+			    cssProperties['width'] = grid.xForLeftEdge() - 5; 
+			}                            
+			
+			$('<span class="y-axis-label">' + formatter(yValues[i]).toString() + '</span>').css(cssProperties).appendTo(this.container);   
 		}     
 		                  
 		return { 
