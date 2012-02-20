b4d7acb68e1e1a66465ae1df3edcf6a20d57407b
Patrick McElhaney <patrick.mcelhaney@ally.com>
2012-02-20 10:11:38 -0500

    Started chart for A/B test, which initallially is a copy of the bar chart

--- js/chart-examples/examples.js
+++ js/chart-examples/examples.js
@@ -8,8 +8,32 @@ define(['../chart/chart', '../math/money'], function (chart, Money) {
 	};
 
 
-	$(function() {
-		var labels = ['1st term (12 mo)', '1 renewal (2 yr)', '2 renewals (3 yr)', '3 renewals (4 yr)'];
+	$(function() {          
+	           
+	    var labels = ['1st term (12 mo)', '1 renewal (2 yr)', '2 renewals (3 yr)', '3 renewals (4 yr)'];       
+	    
+	    $('#abtest-rates').chart({
+			data: [Money(50598.54), Money(51204.25), Money(51817.21), Money(52437.51)],
+			layers: [
+				"borders", 
+				["y-axis markers", Money], 
+				"x-axis label separators",
+				["x-axis labels", ['1st term (12 mo)', '1 renewal (2 yr)', '2 renewals (3 yr)', '3 renewals (4 yr)']], 
+				"bars", 
+				"values above points", 
+				["bubble", function (i, value) { return labels[i] + '<br>Earnings: ' + value.toString(); }],
+				"column hotspots"
+			],
+			marginBottom: 20,
+			marginTop: 40,
+			marginLeft: 100,
+			marginRight: 10,
+			gradients: ['270-rgb(55,152,199)-rgb(70,195,255)' /*blue*/, '270-rgb(101,3,96)-rgb(211,6,201)'/*purple*/]
+			
+		});
+	    
+	    
+
 		
 		$('#bar').chart({
 			data: [Money(50598.54), Money(51204.25), Money(51817.21), Money(52437.51)],
