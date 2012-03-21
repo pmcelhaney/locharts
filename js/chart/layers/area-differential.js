91a620b719ab3c4625778b0c88dca65fc275e47b
Patrick McElhaney <patrick.mcelhaney@ally.com>
2012-03-21 10:15:33 -0400

    Formatted examples script to pass JSHint

--- js/chart/layers/area-differential.js
+++ js/chart/layers/area-differential.js
@@ -1,40 +1,43 @@
-define(function () {
+define(function() {
 
 
-	
 
-	return  function () {
+	return function() {
 		var paper = this.paper;
 		var grid = this.grid;
-		var data = this.data;	
+		var data = this.data;
 
-		var getX = function (index) {
-			return grid.xForIndex(index) + 0.5;
-		};
-		
-		var getY = function (series, index) {
-			if (series < 0) return grid.yForBottomEdge();
-			return grid.yForValue(data[series][index]) + 0.5;
-		};
+		var getX = function(index) {
+				return grid.xForIndex(index) + 0.5;
+			};
+
+		var getY = function(series, index) {
+				if (series < 0) return grid.yForBottomEdge();
+				return grid.yForValue(data[series][index]) + 0.5;
+			};
 
 
-		$(data).each(function (i) {
-		
-			var path = ['M',  getX(i), getY(i, i) ]; // left 
+		$(data).each(function(i) {
+
+			var path = ['M', getX(i), getY(i, i)]; // left 
 			var j = i;
-			
+
 			while (j++ <= data.length) {
-				path.push('L', getX(j), getY(i, j) ); // interior top
+				path.push('L', getX(j), getY(i, j)); // interior top
 			}
 
 			while (j-- >= i) {
-				path.push('L', getX(j), getY(i-1, j)  ); // interior bottom
+				path.push('L', getX(j), getY(i - 1, j)); // interior bottom
 			}
 
-			paper.path(path).attr({	fill: grid.fillColor(i), 'stroke-width': 0, opacity: 0.4 });
+			paper.path(path).attr({
+				fill: grid.fillColor(i),
+				'stroke-width': 0,
+				opacity: 0.4
+			});
 		});
 
-		
+
 	};
 
 });
