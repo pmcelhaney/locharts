e4901402b723e4b1e3db52e7f13243a679f14357
Patrick McElhaney <patrick.mcelhaney@ally.com>
2012-04-02 15:44:22 -0400

    New layer that adds text as HTML, to be styled with CSS.

--- js/chart/layers.js
+++ js/chart/layers.js
@@ -1,6 +1,6 @@
 define(
-	['./layers/bars', './layers/x-axis-labels', './layers/values-above-points', './layers/x-axis-label-separators', './layers/y-axis-markers', './layers/background', './layers/bubble', './layers/horizontal-grid-lines', './layers/dots', './layers/hover-dots', './layers/area', './layers/area-differential', './layers/hotspots', './layers/hotspots-column', './layers/candlestick', './layers/pie', './layers/pie-labels', './layers/x-axis-date-labels', './layers/scrubber'], 
-	function (bars, xAxisLabels, valuesAbovePoints, xAxisLabelSeparators, yAxisMarkers, background, bubble, horizontalGridLines, dots, hoverDots, area, differentialArea, hotspots, columnHotspots, candlestick, pie, pieLabels, xAxisDateLabels, scrubber) {
+	['./layers/bars', './layers/x-axis-labels', './layers/values-above-points', './layers/x-axis-label-separators', './layers/y-axis-markers', './layers/background', './layers/bubble', './layers/horizontal-grid-lines', './layers/dots', './layers/hover-dots', './layers/area', './layers/area-differential', './layers/hotspots', './layers/hotspots-column', './layers/candlestick', './layers/pie', './layers/pie-labels', './layers/x-axis-date-labels', './layers/scrubber', "./layers/values-as-html"],
+	function (bars, xAxisLabels, valuesAbovePoints, xAxisLabelSeparators, yAxisMarkers, background, bubble, horizontalGridLines, dots, hoverDots, area, differentialArea, hotspots, columnHotspots, candlestick, pie, pieLabels, xAxisDateLabels, scrubber, valuesAsHtml) {
 
 
 return {
@@ -22,7 +22,7 @@ return {
 	'pie': pie,
 	'pie labels': pieLabels,
 	'x-axis date labels': xAxisDateLabels,
-	'scrubber': scrubber	
+	'values as html': valuesAsHtml
 };
 
 
