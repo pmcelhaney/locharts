631eed4ba8c6084ea3fb50dfc552d17042d3d787
Patrick McElhaney <patrick.mcelhaney@ally.com>
2012-04-04 09:43:07 -0400

    Cleaned up formatting of unit tests

--- specs/ally-charts-spec.js
+++ specs/ally-charts-spec.js
@@ -1,16 +1,18 @@
-window.logTime = function () {
-	setTimeout( function () { alert("Don't forget to remove calls to logTime()."); }, 1);
-	window.logTime = function () {};	
+window.logTime = function() {
+	setTimeout(function() {
+		alert("Don't forget to remove calls to logTime().");
+	}, 1);
+	window.logTime = function() {};
 };
 
 
 
-define(['js/chart/layers', 'js/chart/chart', './grid-spec', './money-spec' ], function (builtInLayers) {
+define(['js/chart/layers', 'js/chart/chart', './grid-spec', './money-spec'], function(builtInLayers) {
 
-	
+
 
 	describe("Chart Widget", function() {
-	/*
+		/*
 	- yForIndex(i)
 	- xForValue(v)
 	- rowHeight()
@@ -19,152 +21,183 @@ define(['js/chart/layers', 'js/chart/chart', './grid-spec', './money-spec' ], fu
 
 
 
-		window.Raphael = function (c, w, h) { 
-			return { 
+		window.Raphael = function(c, w, h) {
+			return {
 				container: c,
 				width: w,
 				height: h,
-				remove: function () {}
-			}; 
+				remove: function() {}
+			};
 		};
 
-		it("should make the data available to the layer", function () {
+		it("should make the data available to the layer", function() {
 			var data;
-			var layer = function () {
-				 data = this.data;
-			};
+			var layer = function() {
+					data = this.data;
+				};
 
 			$('div').chart({
-				 layers: [ layer ],
-				 data: [ [2, 4, 6, 8] ]
+				layers: [layer],
+				data: [
+					[2, 4, 6, 8]
+				]
 			});
-			
-			expect(data).toEqual([ [2, 4, 6, 8] ]);
+
+			expect(data).toEqual([
+				[2, 4, 6, 8]
+			]);
 		});
-		
-		it("should make the data available to the layer -- when the data is a function", function () {
+
+		it("should make the data available to the layer -- when the data is a function", function() {
 			var data;
-			var layer = function () {
-				 data = this.data;
-			};
+			var layer = function() {
+					data = this.data;
+				};
 
 			$('div').chart({
-				 layers: [ layer ],
-				 data: function () { return	 [2, 4, 6, 8]; }
+				layers: [layer],
+				data: function() {
+					return [2, 4, 6, 8];
+				}
 			});
-			
-			expect(data).toEqual([ [2, 4, 6, 8] ]);
+
+			expect(data).toEqual([
+				[2, 4, 6, 8]
+			]);
 		});
-		
-		
-		it("should make the target element available to the layer", function () {
+
+
+		it("should make the target element available to the layer", function() {
 			var element;
-			var layer = function () {
-				 element = this.element;
-			};
+			var layer = function() {
+					element = this.element;
+				};
 
 			$('<div id="parentElement"></div>').chart({
-				 layers: [ layer ],
-				 data: [ [2, 4, 6, 8] ]
+				layers: [layer],
+				data: [
+					[2, 4, 6, 8]
+				]
 			});
-			
+
 			expect(element.id).toEqual('parentElement');
 		});
-		
-		
-		it("should make the container element available to the layer", function () {
+
+
+		it("should make the container element available to the layer", function() {
 			var container;
-			var layer = function () {
-				 container = this.container;
-			};
+			var layer = function() {
+					container = this.container;
+				};
 
 			var target = $('<div id="parentElement"></div>').chart({
-				 layers: [ layer ],
-				 data: [ [2, 4, 6, 8] ]
+				layers: [layer],
+				data: [
+					[2, 4, 6, 8]
+				]
 			})[0];
-			
+
 			expect(container).toEqual(target.children[0]);
 		});
-		
-		it("should wrap in a relatively-positioned div", function () {
+
+		it("should wrap in a relatively-positioned div", function() {
 			var element;
-			var layer = function () {
-				 element = this.element;
-			};
-			
+			var layer = function() {
+					element = this.element;
+				};
+
 			var container = $('<div id="parentElement"></div>').chart({
-				 layers: [ layer ],
-				 data: [ [2, 4, 6, 8] ]
+				layers: [layer],
+				data: [
+					[2, 4, 6, 8]
+				]
 			});
-			
+
 			expect(container.children().first().css('position')).toEqual('relative');
 		});
-		
-		it("should make the eventTarget available to the layer", function () {
+
+		it("should make the eventTarget available to the layer", function() {
 			var eventTarget;
-			var layer = function () {
-				 eventTarget = this.eventTarget;
-			};
+			var layer = function() {
+					eventTarget = this.eventTarget;
+				};
 
 			$('<div id="parentElement"></div>').chart({
-				 layers: [ layer ],
-				 data: [ [2, 4, 6, 8] ],
-				 eventTarget: '#target'
+				layers: [layer],
+				data: [
+					[2, 4, 6, 8]
+				],
+				eventTarget: '#target'
 			});
-			
+
 			expect(eventTarget).toEqual('#target');
 		});
-	
-		it("should wrap one-dimensional data in an array", function () {
+
+		it("should wrap one-dimensional data in an array", function() {
 			var data;
-			var layer = function () {
-				data = this.data;
-			};
-		
+			var layer = function() {
+					data = this.data;
+				};
+
 			$('<div></div>').chart({
-				layers: [ layer ],
+				layers: [layer],
 				data: [2, 4, 6, 8]
 			});
-		
-			expect(data).toEqual([ [2, 4, 6, 8 ] ]);
+
+			expect(data).toEqual([
+				[2, 4, 6, 8]
+			]);
 		});
-	
-		it("should create a grid with the right height and width", function () {
+
+		it("should create a grid with the right height and width", function() {
 			var grid;
 			$('<div style="width: 600px; height: 400px"></div>').chart({
-					layers: [ function () { } ],
-					data: [],
-					Grid: function (options) { grid = options; }
-			});		 
+				layers: [function() {}],
+				data: [],
+				Grid: function(options) {
+					grid = options;
+				}
+			});
 			expect(grid.width).toEqual(600);
 			expect(grid.height).toEqual(400);
 		});
-	
-		it("should set the yMaxValue to 110% of the highest value in the data", function () {
+
+		it("should set the yMaxValue to 110% of the highest value in the data", function() {
 			var grid;
 			$('<div></div>').chart({
-					layers: [ function () { } ],
-					data: [10,20,30,40,50],
-					Grid: function (options) { grid = options; return { columnWidth: function () { console.debug(arguments.callee.caller); } }; }
+				layers: [function() {}],
+				data: [10, 20, 30, 40, 50],
+				Grid: function(options) {
+					grid = options;
+					return {
+						columnWidth: function() {
+							console.debug(arguments.callee.caller);
+						}
+					};
+				}
 			});
 			expect(grid.yMaxValue).toEqual(55);
 		});
-	
-		it("should pass the yMinValue,	margins, and colors, and other options to the grid", function () {
+
+		it("should pass the yMinValue,	margins, and colors, and other options to the grid", function() {
 			var grid;
 			$('<div></div>').chart({
-				
-				Grid: function (options) { return options; },
+
+				Grid: function(options) {
+					return options;
+				},
 
 				layers: [
-					function () {
-						grid = this.grid;
-					}
+
+				function() {
+					grid = this.grid;
+				}],
+
+				data: [
+					[10, 20, 50]
 				],
-			
-				data: [ [10, 20, 50] ],
-				yMinValue: 5, 
-				xValues: [1,2,3],
+				yMinValue: 5,
+				xValues: [1, 2, 3],
 				marginTop: 10,
 				marginRight: 20,
 				marginBottom: 30,
@@ -173,12 +206,12 @@ define(['js/chart/layers', 'js/chart/chart', './grid-spec', './money-spec' ], fu
 				fillColors: ['#111', '#222'],
 				gradients: ['0-#aaa-#bbb', '0-#111-#222'],
 				edgeToEdge: true
-				
+
 			});
-			
-		  
+
+
 			expect(grid.yMinValue).toEqual(5);
-			expect(grid.xValues).toEqual([1,2,3]);
+			expect(grid.xValues).toEqual([1, 2, 3]);
 			expect(grid.marginTop).toEqual(10);
 			expect(grid.marginRight).toEqual(20);
 			expect(grid.marginBottom).toEqual(30);
@@ -188,136 +221,142 @@ define(['js/chart/layers', 'js/chart/chart', './grid-spec', './money-spec' ], fu
 			expect(grid.fillColors).toEqual(['#111', '#222']);
 			expect(grid.gradients).toEqual(['0-#aaa-#bbb', '0-#111-#222']);
 			expect(grid.edgeToEdge).toBeTruthy();
-		
+
 		});
-		
-		it("should pass the columnCount to the grid", function () {
+
+		it("should pass the columnCount to the grid", function() {
 			var grid;
 			$('<div></div>').chart({
-				
-				Grid: function (options) { return options; },
+
+				Grid: function(options) {
+					return options;
+				},
 
 				layers: [
-					function () {
-						grid = this.grid;
-					}
-				],
-			
-				data: [ [10, 20, 50] ]
-				
+
+				function() {
+					grid = this.grid;
+				}],
+
+				data: [
+					[10, 20, 50]
+				]
+
 			});
-			
-		  
+
+
 			expect(grid.columnCount).toEqual(3);
-		
+
 		});
-		
-	
-		it("should apply each layer in order", function () {
+
+
+		it("should apply each layer in order", function() {
 			var layers = [];
-			var Layer = function (name) {
-				return function () {
-					layers.push(name);
-				};	  
-			};
+			var Layer = function(name) {
+					return function() {
+						layers.push(name);
+					};
+				};
 			$('<div></div>').chart({
 				layers: [Layer('one'), Layer('two'), Layer('three')]
 			});
 			expect(layers).toEqual(['one', 'two', 'three']);
 		});
-	
+
 		it("should pass the options for a layer", function() {
 			var args;
-			var layer = function () {
-				args = Array.prototype.slice.apply(arguments);
-			};
+			var layer = function() {
+					args = Array.prototype.slice.apply(arguments);
+				};
 			$('<div></div>').chart({
-				layers: [ [layer, 'arg1', 'arg2'] ]
+				layers: [
+					[layer, 'arg1', 'arg2']
+				]
 			});
 			expect(args).toEqual(['arg1', 'arg2']);
 		});
-	
+
 		it("should recognize a built-in layer and skip a missing layer", function() {
-			builtInLayers['a built-in layer'] = function () {
+			builtInLayers['a built-in layer'] = function() {
 				args = Array.prototype.slice.apply(arguments);
 			};
-		
+
 			$('<div></div>').chart({
-				layers: [ 'a built-in layer', 'a missing layer', ['a built-in layer', 'foo', 'bar'] ]
-			});
-		
-			expect(args).toEqual(['foo', 'bar']);
-		});
-		
-		
-		it("should draw a bar chart by default", function () {
-			var layers = [];
-			var defaultLayers = ['borders','y-axis markers', 'bars','values above points'];
-			
-			$(defaultLayers).each(function (i, name) {
-				builtInLayers[name] = function () { layers.push(name); };
+				layers: ['a built-in layer', 'a missing layer', ['a built-in layer', 'foo', 'bar']]
 			});
 
-			
+			expect(args).toEqual(['foo', 'bar']);
+		});
+
+
+		it("should draw a bar chart by default", function() {
+			var layers = [];
+			var defaultLayers = ['borders', 'y-axis markers', 'bars', 'values above points'];
+
+			$(defaultLayers).each(function(i, name) {
+				builtInLayers[name] = function() {
+					layers.push(name);
+				};
+			});
+
+
 			$('<div></div>').chart({
 				data: [1, 2, 3]
 			});
-			
+
 			expect(layers).toEqual(defaultLayers);
-			
+
 		});
-		
-		
-		it("should remove each layer when the chart is removed", function () {
+
+
+		it("should remove each layer when the chart is removed", function() {
 			var removedLayers = [];
-			var Layer = function (name) {
-				return function () {
-					return {
-						remove: function() {
-						   removedLayers.push(name); 
-						}
+			var Layer = function(name) {
+					return function() {
+						return {
+							remove: function() {
+								removedLayers.push(name);
+							}
+						};
 					};
-				};	  
-			};
+				};
 			$('<div></div>').chart({
 				layers: [Layer('one'), Layer('two'), Layer('three')]
 			}).chart('remove');
-			
-			
+
+
 			expect(removedLayers).toEqual(['one', 'two', 'three']);
 		});
-	
-	
-		describe("the paper", function () {
-		
+
+
+		describe("the paper", function() {
+
 			var paper;
 			var element = $('<div style="width: 600px; height: 400px"></div');
-		
-		
-			beforeEach(function () {
-				var layer = function () {
-					paper = this.paper;
-				};
-			
+
+
+			beforeEach(function() {
+				var layer = function() {
+						paper = this.paper;
+					};
+
 				element.chart({
-					layers: [ layer ]
+					layers: [layer]
 				});
-			
+
 			});
-		
+
 			it("should be added to the relatively positioned wrapper inside the target element", function() {
 				expect(paper.container).toEqual(element[0].children[0]);
 			});
-	
-			it("should have the width and height of the target element", function () {
+
+			it("should have the width and height of the target element", function() {
 				expect(paper.width).toEqual(600);
 				expect(paper.height).toEqual(400);
 			});
-	
+
 		});
-	
+
 	});
 
 });
-
-
