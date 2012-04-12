5df50145ab469485669c9a5cd6d41d9091a76345
Patrick McElhaney <pmcelhaney@gmail.com>
2012-04-12 17:36:25 -0400

    line chart doens't need fillColors

--- js/chart-examples/line.js
+++ js/chart-examples/line.js
@@ -7,12 +7,13 @@ define(['../chart/chart', '../chart/layers/line-chart', '../math/money'], functi
         $('#line').chart({
             data: [money(50598.54), money(51204.25), money(51817.21), money(52437.51)],
             layers: [ lineChart ],
-            spec: { labels: ['1st term (12 mo)', '1 renewal (2 yr)', '2 renewals (3 yr)', '3 renewals (4 yr)'] },
+            spec: {
+                labels: ['1st term (12 mo)', '1 renewal (2 yr)', '2 renewals (3 yr)', '3 renewals (4 yr)']
+            },
             marginBottom: 20,
             marginTop: 40,
             marginLeft: 100,
-            marginRight: 10,
-            fillColors: ['rgb(55,152,199)', 'rgb(101,3,96)']
+            marginRight: 10
         });
     });
 });
